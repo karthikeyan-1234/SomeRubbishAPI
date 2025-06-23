c36d30bcc2af7a6a40c9f706028253cf73c1ed6d
karthikeyan-1234 <mail2karthikkn@gmail.com>
2025-06-23 19:40:42 +0530

    Removed https redirection as it was causing issues in local IIS

--- SomeRubbishAPI/Program.cs
+++ SomeRubbishAPI/Program.cs
@@ -36,7 +36,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
